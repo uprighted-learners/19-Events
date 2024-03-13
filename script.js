43902c3d75baea9de3f4b6ba9fa8e3a252a167a3
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-03-13 18:19:33 -0500

    more buttons

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // create a node from the html element with an id of btn
 let button = document.getElementById('btn');
+let prestoButton = document.getElementById("presto");
+let abraButton = document.getElementById("abra");
 
 // add an event listener to the button
-button.addEventListener("click", () => {
-    console.log("before abracadabra")
-    alert("Abracadabra!")
-    console.log("after abracadabra")
-});
+button.addEventListener("click", sayMagicWord);
+
+function sayMagicWord(event){
+    if(event.target.id === "presto"){
+        alert("Change-o!")
+    } else if (event.target.id === "abra"){
+        alert("Cadabra!")
+    } else {
+        alert("Shazam!")
+    }
+    console.log({event})
+}
+
+prestoButton.addEventListener("click", sayMagicWord)
+abraButton.addEventListener("click", sayMagicWord)
