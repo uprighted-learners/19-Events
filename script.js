a0615b087bf510880b9cadf5ba6aefbb4f66a272
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-03-13 17:49:17 -0500

    magic button example

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("we are doing events!")
+// create a node from the html element with an id of btn
+let button = document.getElementById('btn');
+
+// add an event listener to the button
+button.addEventListener("click", () => {
+    console.log("before abracadabra")
+    alert("Abracadabra!")
+    console.log("after abracadabra")
+});
